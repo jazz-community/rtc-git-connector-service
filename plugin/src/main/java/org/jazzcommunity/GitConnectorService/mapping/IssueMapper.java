d9b1a99d72ec15d2538db8e4fae08d3097273f53
SBI- <pascal.huerlimann@gmail.com>
2018-06-04 13:57:58 +0200

    Add oslc state converter

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/mapping/IssueMapper.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/mapping/IssueMapper.java
@@ -14,6 +14,8 @@ public class IssueMapper {
     public static ModelMapper get() {
         final ModelMapper mapper = new ModelMapper();
 
+        // I think a log of these converters can be extracted and reused, especially the ones that will
+        // need to be written for deep mapping of objects like links etc.
         final AbstractConverter<Collection<String>, String> listToString =
                 new AbstractConverter<Collection<String>, String>() {
                     @Override
@@ -27,6 +29,13 @@ public class IssueMapper {
             @Override
             protected String convert(Integer iid) {
                 return "Issue " + iid;
+            }
+        };
+
+        final AbstractConverter<String, Boolean> stateConverter = new AbstractConverter<String, Boolean>() {
+            @Override
+            protected Boolean convert(String state) {
+                return state != null;
             }
         };
 
@@ -48,10 +57,13 @@ public class IssueMapper {
                 using(listToString).map(source.getLabels()).setDctermsSubject(null);
                 map().setGitCmLabels(source.getLabels());
 
+                // rdf:about needs to be the url of this link, so something like
+                // https://localhost:7443/jazz/service/org.jazzcommunity.GitConnectorService.IGitConnectorService/gitlab/code.siemens.com/project/13027/issue/9/link
+
                 // dates left out for now.
 
                 // does this need a custom converter?
-                map().setOslcCmClosed(source.getClosedAt() != null);
+                using(stateConverter).map(source.getClosedAt()).setOslcCmClosed(null);
 
                 // Todos skipped
 
@@ -61,6 +73,27 @@ public class IssueMapper {
                 // prefixes skipped, this will need to be a custom object defined separately
 
                 using(shortTitleConverter).map(source.getIid()).setOslcShortTitle(null);
+
+                // Lots of properties skipped which are not definitively defined
+
+                map().setGitCmProjectId(source.getProjectId());
+
+                // milestone skipped because deep object not defined yet
+                // same with assignees and author
+
+                map().setGitCmUserNotesCount(source.getUserNotesCount());
+                map().setGitCmUpvotes(source.getUpvotes());
+                map().setGitCmDownvotes(source.getDownvotes());
+                map().setGitCmDueDate(source.getDueDate());
+                map().setGitCmConfidential(source.getConfidential());
+                // Weight skipped, EE feature only. Not sure if we need this.
+                map().setGitCmDiscussionLocked(source.getDiscussionLocked());
+                map().setGitCmWebUrl(source.getWebUrl());
+
+                // time stats skipped, deep mapping undefined
+
+                // links skipped, deep mapping not implemented
+                map().setGitCmSubscribed(source.getSubscribed());
             }
         });
 
