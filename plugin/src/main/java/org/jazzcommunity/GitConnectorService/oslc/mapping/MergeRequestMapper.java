0f91c794500b0fbc7f633a55d58d820ddc136331
SBI- <pascal.huerlimann@gmail.com>
2018-06-21 15:22:28 +0200

    Implement empty mapping

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/oslc/mapping/MergeRequestMapper.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/oslc/mapping/MergeRequestMapper.java
@@ -1,6 +1,15 @@
 package org.jazzcommunity.GitConnectorService.oslc.mapping;
 
+import ch.sbi.minigit.type.gitlab.mergerequest.MergeRequest;
+import org.jazzcommunity.GitConnectorService.olsc.type.issue.OslcMergeRequest;
+import org.modelmapper.ModelMapper;
+
 public class MergeRequestMapper {
     private MergeRequestMapper() {
+    }
+
+    public static OslcMergeRequest map(MergeRequest request) {
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(request, OslcMergeRequest.class);
     }
 }
