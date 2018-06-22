ab24df8c0a60a88d533f0d96c812d015015b1365
SBI- <pascal.huerlimann@gmail.com>
2018-06-22 15:48:15 +0200

    Remove non tokenized mapping

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/oslc/mapping/TypeConverter.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/oslc/mapping/TypeConverter.java
@@ -2,6 +2,7 @@ package org.jazzcommunity.GitConnectorService.oslc.mapping;
 
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 
 import java.lang.reflect.Type;
 
@@ -9,24 +10,10 @@ public final class TypeConverter {
     private TypeConverter() {
     }
 
-    // Also, maybe instead of passing a class, I want to Pass a type here...
-    public static <From, To> AbstractConverter<From, To> to(final Class<To> to) {
+    public static <From, To> AbstractConverter<From, To> to(final Type type) {
         // It would be nice to have a reflective check here to see if the
         // two types are actually compatible... Or maybe find out how
         // to define inheritance with auto-generated types.
-        return new AbstractConverter<From, To>() {
-            @Override
-            protected To convert(From from) {
-                if (from == null) {
-                    return null;
-                }
-
-                return new ModelMapper().map(from, to);
-            }
-        };
-    }
-
-    public static <From, To> AbstractConverter<From, To> to(final Type type) {
         return new AbstractConverter<From, To>() {
             @Override
             protected To convert(From from) {
