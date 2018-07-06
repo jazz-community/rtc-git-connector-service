9e75b9a7573a906c546fe2fe6e2c08db2b2b76b7
SBI- <pascal.huerlimann@gmail.com>
2018-07-06 17:51:00 +0200

    Add image service using guava resources

---
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/service/resource/ImageService.java
@@ -0,0 +1,26 @@
+package org.jazzcommunity.GitConnectorService.service.resource;
+
+import com.google.common.io.ByteSource;
+import com.google.common.io.Resources;
+import com.ibm.team.repository.service.TeamRawService;
+import com.siemens.bt.jazz.services.base.rest.parameters.PathParameters;
+import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
+import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
+import org.apache.commons.logging.Log;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URL;
+
+public class ImageService extends AbstractRestService {
+    public ImageService(Log log, HttpServletRequest request, HttpServletResponse response, RestRequest restRequest, TeamRawService parentService, PathParameters pathParameters) {
+        super(log, request, response, restRequest, parentService, pathParameters);
+    }
+
+    @Override
+    public void execute() throws Exception {
+        URL url = Resources.getResource("images/issue_github_16x16.png");
+        ByteSource source = Resources.asByteSource(url);
+        source.copyTo(response.getOutputStream());
+    }
+}
