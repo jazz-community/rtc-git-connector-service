31394c47a5c6b81bb36e576c484923e27563fb2a
SBI- <pascal.huerlimann@gmail.com>
2018-07-19 09:25:28 +0200

    Use naked url connection for demonstration

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/service/proxy/ProxyService.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/service/proxy/ProxyService.java
@@ -1,21 +1,15 @@
 package org.jazzcommunity.GitConnectorService.service.proxy;
 
-import com.google.common.io.CharStreams;
+import com.google.common.io.ByteStreams;
 import com.ibm.team.repository.service.TeamRawService;
 import com.siemens.bt.jazz.services.base.rest.parameters.PathParameters;
 import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
 import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Enumeration;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 public class ProxyService extends AbstractRestService {
 
@@ -34,23 +28,30 @@ public class ProxyService extends AbstractRestService {
     String host = pathParameters.get("host");
     // it would be nice if base service offered a "rest" of the url
     String rest = restRequest.toString().substring("proxy/".length() + host.length() + 1);
-    String url = String.format("https://%s/%s", host, rest);
+    String requestUrl = String.format("https://%s/%s", host, rest);
 
-    HttpUriRequest apiRequest = RequestBuilder.get(url).build();
+    System.out.println(requestUrl);
 
-    Enumeration<String> names = request.getHeaderNames();
-    while (names.hasMoreElements()) {
-      String header = names.nextElement();
-      apiRequest.addHeader(header, request.getHeader(header));
-    }
+    URL url = new URL(requestUrl);
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.connect();
+    ByteStreams.copy(connection.getInputStream(), response.getOutputStream());
 
-    CloseableHttpClient client = HttpClientBuilder.create().build();
-    try (CloseableHttpResponse apiResponse = client.execute(apiRequest);
-        InputStream content = apiResponse.getEntity().getContent()) {
-      String result = CharStreams.toString(new InputStreamReader(content, "UTF-8"));
-      System.out.println(result);
-      // set headers in response
-      response.getWriter().write(result);
-    }
+    //    HttpUriRequest apiRequest = RequestBuilder.get(url).build();
+    //
+    //    Enumeration<String> names = request.getHeaderNames();
+    //    while (names.hasMoreElements()) {
+    //      String header = names.nextElement();
+    //      apiRequest.addHeader(header, request.getHeader(header));
+    //    }
+    //
+    //    CloseableHttpClient client = HttpClientBuilder.create().build();
+    //    try (CloseableHttpResponse apiResponse = client.execute(apiRequest);
+    //        InputStream content = apiResponse.getEntity().getContent()) {
+    //      String result = CharStreams.toString(new InputStreamReader(content, "UTF-8"));
+    //      System.out.println(result);
+    //      // set headers in response
+    //      response.getWriter().write(result);
+    //    }
   }
 }
