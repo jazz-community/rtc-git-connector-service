cddf080f0e0719aff3cccb60e0dffb100389076c
SBI- <pascal.huerlimann@gmail.com>
2019-02-13 07:43:19 +0100

    Extract commit service debugging functions

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/ccm/net/Request.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/ccm/net/Request.java
@@ -1,10 +1,13 @@
 package org.jazzcommunity.GitConnectorService.ccm.net;
 
+import com.google.common.base.Joiner;
 import com.google.common.net.MediaType;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Map.Entry;
 import javax.servlet.http.HttpServletRequest;
 import org.jazzcommunity.GitConnectorService.ccm.properties.PropertyReader;
+import org.apache.commons.logging.Log;
 
 public class Request {
   public static boolean isLinkRequest(HttpServletRequest request) throws IOException {
@@ -17,13 +20,6 @@ public class Request {
     return MediaType.JSON_UTF_8.toString().contains(request.getHeader("Accept"));
   }
 
-  // for debugging
-  public static void printHeader(HttpServletRequest request) {
-    for (String key : Collections.list(request.getHeaderNames())) {
-      System.out.println(String.format("key: %s, value: %s", key, request.getHeader(key)));
-    }
-  }
-
   public static boolean isUrlHoverRequest(HttpServletRequest request) {
     if (request.getParameter("_context") == null
         || request.getParameter("_selector") == null
@@ -33,5 +29,25 @@ public class Request {
     return request.getParameter("_context").contains("web")
         && request.getParameter("_selector").contains("Hover")
         && request.getParameter("_mediaType").contains("text/html");
+  }
+
+  // for debugging
+  public static void logHeader(Log log, HttpServletRequest request) {
+    for (String key : Collections.list(request.getHeaderNames())) {
+      log.debug(String.format("key: %s, value: %s", key, request.getHeader(key)));
+    }
+  }
+
+  public static void logRemote(Log log, HttpServletRequest request) {
+    log.debug(String.format("Remote User: %s", request.getRemoteUser()));
+    log.debug(String.format("Request Uri: %s", request.getRequestURI()));
+  }
+
+  public static void logParameters(Log log, HttpServletRequest request) {
+    for (Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
+      String value = Joiner.on(", ").join(entry.getValue());
+      String out = String.format("Parameter %s: %s", entry.getKey(), value);
+      log.debug(out);
+    }
   }
 }
