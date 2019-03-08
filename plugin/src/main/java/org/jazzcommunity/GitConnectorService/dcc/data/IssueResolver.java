4c6ae23b4677ae84f93fe978d41dba32eedc8ba1
SBI- <pascal.huerlimann@gmail.com>
2019-03-08 15:39:23 +0100

    Use minigit 1.2.0 to enable reporting only against public repositories

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/data/IssueResolver.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/data/IssueResolver.java
@@ -23,21 +23,7 @@ public class IssueResolver implements Resolver<Issue> {
 
   @Override
   public LinkedIssue resolve(UUID projectArea) {
-    // this will fetch data from gitlab/hub or wherever, and return a data object. Which data will
-    // be used for what payloads hasn't been defined yet, so this will just print some data instead
-    // for now.
-    // Most likely, this will be integrated with the git connector service, and therefor api keys
-    // will be retrieved locally. The token saved here is just for development, and to be removed
-    // before open sourcing / merging into the service.
-    // If key retrieval were a library, *cough*, or custom service retrieval were actually possible,
-    // (no, it's not...), this would be super easy.
-    String token = "gT9maFSo4VjTQqu6eXeb";
-    // also, make the api always handle the protocol, if it is not part of the passed in url. Should
-    // always be https anyway.
-    // also, probably, the api should have a factory for which kind of service is being accessed,
-    // github
-    // or gitlab.
-    GitlabApi api = new GitlabApi("https://" + remoteUrl.getServiceUrl(), token);
+    GitlabApi api = new GitlabApi("https://" + remoteUrl.getServiceUrl());
 
     // as well as that, this differentiation should probably be handled by the factory, if we are
     // working with issues or requests or whatever and just try to fetch the proper payload.
