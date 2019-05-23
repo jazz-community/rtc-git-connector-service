a7144d45783b52c64d84e047087a6f1002fbfedc
SBI- <pascal.huerlimann@gmail.com>
2019-05-23 20:53:25 +0200

    Show url lookup for issue links

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/service/IssueLookupTestService.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/service/IssueLookupTestService.java
@@ -1,11 +1,14 @@
 package org.jazzcommunity.GitConnectorService.dcc.service;
 
+import ch.sbi.minigit.gitlab.GitlabApi;
 import com.ibm.team.git.common.internal.IGitRepositoryRegistrationService;
 import com.ibm.team.git.common.model.IGitRepositoryDescriptor;
 import com.ibm.team.repository.service.TeamRawService;
 import com.siemens.bt.jazz.services.base.rest.parameters.PathParameters;
 import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
 import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
+import java.net.URL;
+import java.net.URLEncoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
@@ -31,9 +34,26 @@ public class IssueLookupTestService extends AbstractRestService {
         service.getAllRegisteredGitRepositories(null, null, true, true);
 
     for (IGitRepositoryDescriptor repository : repositories) {
-      System.out.println(repository.getName());
-    }
+      URL url = new URL(repository.getUrl());
+      String encodedProject =
+          URLEncoder.encode(
+              url.getPath().replaceAll(".git$", "").replaceFirst("^\\/", ""), "UTF-8");
+      GitlabApi api = new GitlabApi(String.format("%s://%s", url.getProtocol(), url.getHost()));
 
-    response.getWriter().write("Yep, working");
+      String output =
+          String.format(
+              "Name: %s Link: %s Host: %s://%s Path: %s Project: %s",
+              repository.getName(),
+              repository.getUrl(),
+              url.getProtocol(),
+              url.getHost(),
+              url.getPath(),
+              encodedProject);
+
+      response.getWriter().write(output);
+
+      //      Project project = api.getProject(encodedProject);
+      //      response.getWriter().write(project.getId());
+    }
   }
 }
