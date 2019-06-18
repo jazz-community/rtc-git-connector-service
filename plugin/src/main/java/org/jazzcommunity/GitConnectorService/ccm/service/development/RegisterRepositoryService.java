5009a5b7c145e6562cd85eb341c34a959e4fc83b
SBI- <pascal.huerlimann@gmail.com>
2019-06-18 10:15:59 +0200

    Add repository class and parsing from json

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/ccm/service/development/RegisterRepositoryService.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/ccm/service/development/RegisterRepositoryService.java
@@ -1,15 +1,22 @@
 package org.jazzcommunity.GitConnectorService.ccm.service.development;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.ibm.team.repository.service.TeamRawService;
 import com.siemens.bt.jazz.services.base.rest.parameters.PathParameters;
 import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
 import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
+import com.siemens.bt.jazz.services.base.utils.RequestReader;
 import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
+import org.apache.http.entity.ContentType;
+import org.jazzcommunity.GitConnectorService.ccm.data.GitRepository;
 
 public class RegisterRepositoryService extends AbstractRestService {
+
+  private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
   public RegisterRepositoryService(
       Log log,
@@ -27,10 +34,15 @@ public class RegisterRepositoryService extends AbstractRestService {
 
     if (!url.getHost().equals("localhost")) {
       response.setStatus(403);
+      response.setContentType(ContentType.TEXT_HTML.toString());
       response.getWriter().write("Operation only supported during development.");
       return;
     }
 
-    System.out.println(url.getHost());
+    String raw = RequestReader.readAsString(request);
+    GitRepository[] repositories = gson.fromJson(raw, GitRepository[].class);
+    for (GitRepository repository : repositories) {
+      System.out.println(String.format("name: %s, url: %s", repository.getName(), repository.getUrl()));
+    }
   }
 }
