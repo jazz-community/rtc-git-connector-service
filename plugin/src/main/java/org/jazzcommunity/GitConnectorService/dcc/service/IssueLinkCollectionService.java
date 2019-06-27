ebf5e3bc23aab8d6a163f335b02ed45f1734c5f4
SBI- <pascal.huerlimann@gmail.com>
2019-06-27 10:34:11 +0200

    Implement basic link service example

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/service/IssueLinkCollectionService.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/service/IssueLinkCollectionService.java
@@ -6,8 +6,12 @@ import com.siemens.bt.jazz.services.base.rest.parameters.RestRequest;
 import com.siemens.bt.jazz.services.base.rest.service.AbstractRestService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 import org.apache.commons.logging.Log;
 import org.apache.http.entity.ContentType;
+import org.jazzcommunity.GitConnectorService.dcc.xml.IssueLink;
+import org.jazzcommunity.GitConnectorService.dcc.xml.IssueLinks;
 
 public class IssueLinkCollectionService extends AbstractRestService {
 
@@ -25,5 +29,16 @@ public class IssueLinkCollectionService extends AbstractRestService {
   public void execute() throws Exception {
     response.setContentType(ContentType.APPLICATION_XML.toString());
     response.setCharacterEncoding("UTF-8");
+
+    IssueLinks links = new IssueLinks();
+
+    for (int i = 0; i < 10; i += 1) {
+      IssueLink link = new IssueLink("_bla", "123472", 111, i);
+      links.addLink(link);
+    }
+
+    Marshaller marshaller = JAXBContext.newInstance(IssueLinks.class).createMarshaller();
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    marshaller.marshal(links, response.getWriter());
   }
 }
--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/xml/IssueLinks.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/xml/IssueLinks.java
@@ -1,10 +1,12 @@
 package org.jazzcommunity.GitConnectorService.dcc.xml;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -12,8 +14,16 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class IssueLinks {
   @XmlAttribute private String href;
 
+  @XmlElement(name = "issue")
   private List<IssueLink> links = new ArrayList<>();
 
   @XmlAttribute private String rel = "next";
 
+  public void addLink(IssueLink link) {
+    this.links.add(link);
+  }
+
+  public void addLinks(Collection<IssueLink> links) {
+    this.links.addAll(links);
+  }
 }
