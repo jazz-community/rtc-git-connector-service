1b2dc27fde6318e6f8cd91c5b4039592939e76ff
SBI- <pascal.huerlimann@gmail.com>
2019-12-04 12:56:41 +0100

    Add issue view for reporting building

---
+++ reporting/sql_db2_derby/views/create_issue_view.sql
@@ -0,0 +1,33 @@
+CREATE VIEW RIDW.VW_GIT_ISSUE (
+    NAME,
+    EXTERNAL_LINK,
+    DESCRIPTION,
+    AUTHOR_EMAIL,
+    AUTHOR_NAME,
+    STATE,
+    ID,
+    PROJECT_ID,
+    IID,
+    CREATED,
+    UPDATED,
+    PK
+) AS ( SELECT
+        I.TITLE,
+        I.WEB_URL,
+        I.DESCRIPTION,
+        R.NAME,
+        R.FULL_NAME,
+        I.STATE,
+        I.ID,
+        I.PROJECT_ID,
+        I.IID,
+        I.CREATED,
+        I.UPDATED,
+        I.ID_PK
+    FROM
+        RIODS.GIT_ISSUE I
+    JOIN
+        RIODS.RESOURCE R
+    ON
+        I.RTC_AUTHOR = R.RESOURCE_ID
+);
