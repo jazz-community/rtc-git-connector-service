d5542a8aadc9f4e30a7de94a4e2b61d701c204ec
SBI- <pascal.huerlimann@gmail.com>
2019-12-05 17:02:20 +0100

    Add guard for possibly malformed urls

--- plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/net/UrlParser.java
+++ plugin/src/main/java/org/jazzcommunity/GitConnectorService/dcc/net/UrlParser.java
@@ -30,16 +30,20 @@ public class UrlParser {
       ".*\\/([^\\/]*)\\/([^\\/]*)\\/project\\/([^\\/]*)\\/([^\\/]*)\\/([^\\/]*)\\/link$";
 
   public static RemoteUrl parseRemote(URI uri) {
-    Pattern pattern = Pattern.compile(REMOTE_PATTERN);
-    Matcher matcher = pattern.matcher(uri.getPath());
-    matcher.find();
-    return new RemoteUrl(
-        uri,
-        matcher.group(1),
-        matcher.group(2),
-        matcher.group(3),
-        matcher.group(4),
-        matcher.group(5));
+    try {
+      Pattern pattern = Pattern.compile(REMOTE_PATTERN);
+      Matcher matcher = pattern.matcher(uri.getPath());
+      matcher.find();
+      return new RemoteUrl(
+          uri,
+          matcher.group(1),
+          matcher.group(2),
+          matcher.group(3),
+          matcher.group(4),
+          matcher.group(5));
+    } catch (Exception e) {
+      return new RemoteUrl(uri, "invalid", "invalid", "invalid", "invalid", "invalid");
+    }
   }
 
   public static DataUrl parseData(URI uri) {
