2867f72dabfe0f15049799088b424a95b64e2258
SBI- <pascal.huerlimann@gmail.com>
2019-12-11 15:31:13 +0100

    Add merge request relation table

---
+++ reporting/sql_db2_derby/views/create_merge_request_relation_view.sql
@@ -0,0 +1,20 @@
+CREATE VIEW RIDW.VW_GIT_MERGE_REQUEST_RELATION (
+    MERGE_REQUEST_ID,
+    MERGE_REQUEST_TITLE,
+    REQUEST_ID,
+    REQUEST_NAME,
+    PROJECT_ID,
+    PROJECT_NAME
+) AS (SELECT
+        mr.ID_PK,
+        mr.TITLE,
+        request.REQUEST_ID,
+        request.NAME,
+        project.PROJECT_ID,
+        project.NAME
+    FROM RIODS.GIT_ISSUE_RELATION relation
+    JOIN RIODS.GIT_MERGE_REQUEST mr ON mr.ID_PK = relation.MERGE_REQUEST_ID
+    JOIN RIODS.REQUEST request ON request.REQUEST_ID = relation.REQUEST_ID
+    JOIN RIODS.PROJECT project ON project.PROJECT_ID = relation.PROJECT_ID
+);
+
