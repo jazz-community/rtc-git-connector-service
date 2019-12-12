6b39b585957f7e4e60005a040cbe96492c366a20
SBI- <pascal.huerlimann@gmail.com>
2019-12-12 15:23:00 +0100

    Move docker related files to seperate folder

---
+++ reporting/sql_db2_derby/docker/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# this script is for testing that schema, views and migrations
+# are valid db2 syntax.
+
+# run scripts with db user and against the default database
+#printf "admin" | su - db2inst1
+#. /home/db2inst1/sqllib/db2profile
+#/home/db2inst1/sqllib/bin/db2 connect to db2inst1
+
+/home/db2inst1/sqllib/bin/db2 connect to db2inst1 user db2inst1 using admin
+
+# create reporting schemas
+/home/db2inst1/sqllib/bin/db2 CREATE SCHEMA RIODS
+/home/db2inst1/sqllib/bin/db2 CREATE SCHEMA RIDW
+
+# # create commit tables
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_commit_table.sql
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_commit_lookup_table.sql
+# # create issue tables
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_issue_table.sql
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_issue_assignee_table.sql
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_issue_relation_table.sql
+# # create merge request tables
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_merge_request_table.sql
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_merge_request_assignee_table.sql
+# /home/db2inst1/sqllib/bin/db2 -tvf /var/custom/tables/create_merge_request_relation_table.sql
+
+/home/db2inst1/sqllib/bin/db2 connect reset
