0fedf75ed9a9c3b869a4e5f8333543684e5c149b
SBI- <pascal.huerlimann@gmail.com>
2019-12-12 11:09:00 +0100

    Enable local volume mounting

--- reporting/sql_db2_derby/run.ps1
+++ reporting/sql_db2_derby/run.ps1
@@ -3,5 +3,6 @@ docker run -itd --name db2test `
     -p 50000:50000 `
     -e LICENSE=accept `
     -e DB2INST1_PASSWORD=admin `
-    -e PERSISTENT_HOME=false
+    -e PERSISTENT_HOME=false `
+    -v ${PWD}/test-volume:/database `
     db2integration
--- reporting/sql_db2_derby/setup.sh
+++ reporting/sql_db2_derby/setup.sh
@@ -4,17 +4,17 @@
 su - db2inst1
 
 # create reporting databases
-db2 CREATE DATABASE RIODS
-db2 CREATE DATABASE RIDW
+/home/db2inst1/sqllib/bin/db2 CREATE DATABASE RIODS
+/home/db2inst1/sqllib/bin/db2 CREATE DATABASE RIDW
 
 # create commit tables
-db2 -tvf tables/create_commit_table.sql
-db2 -tvf tables/create_commit_lookup_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_commit_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_commit_lookup_table.sql
 # create issue tables
-db2 -tvf tables/create_issue_table.sql
-db2 -tvf tables/create_issue_assignee_table.sql
-db2 -tvf tables/create_issue_relation_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_issue_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_issue_assignee_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_issue_relation_table.sql
 # create merge request tables
-db2 -tvf tables/create_merge_request_table.sql
-db2 -tvf tables/create_merge_request_assignee_table.sql
-db2 -tvf tables/create_merge_request_relation_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_merge_request_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_merge_request_assignee_table.sql
+/home/db2inst1/sqllib/bin/db2 -tvf tables/create_merge_request_relation_table.sql
